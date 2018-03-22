fa3575e8f506f09318e0d3b3a93425d5d67b76f1
weekendyy <weekend.q@gmail.com>
2018-03-22 10:09:54 +0800

    增加loading

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,12 +2,6 @@ class Config {
   constructor(){
   }
 }
-Config.resUrl = 'https://api.czsjcrm.cn/'
-Config.shopID = '348'
-//厦门橙子集卡
-// Config.shopID = '394'
-//魔法卡007
-// Config.shopID = '393'
-//西瓜砍价
-// Config.shopID = '392'
+Config.resUrl = 'https://api.czsjcrm.cn/';
+Config.shopID = '348';
 export default Config
